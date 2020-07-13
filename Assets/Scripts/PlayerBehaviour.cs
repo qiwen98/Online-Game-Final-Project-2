5f3d8d302a4b5c545ccdf19969bbf5dddf7fbaa5
qiwen98 <45889437+qiwen98@users.noreply.github.com>
2020-07-13 15:49:52 +0800

    update 3

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -331,8 +331,7 @@ public class PlayerBehaviour : MonoBehaviourPunCallbacks
                  {
                     // Debug.Log("updatescore");
                     UpdateScore(other.gameObject, ScoreManager.instance.gunScore);
-                   
-                    if (!other.gameObject.transform.parent.GetComponent<PhotonView>().IsMine)
+                    if(!other.gameObject.transform.parent.GetComponent<PhotonView>().IsMine)
                     {
                         TakeDamage(1f, PhotonNetwork.LocalPlayer);
                     }
@@ -424,6 +423,7 @@ public class PlayerBehaviour : MonoBehaviourPunCallbacks
 
     void UpdateScore(GameObject trap, float trapType_score)
     {
+        bool iswritten = false;
         if (trap.gameObject.transform.parent.GetComponent<PhotonView>().Owner== PhotonNetwork.LocalPlayer)
         {
            
@@ -432,7 +432,7 @@ public class PlayerBehaviour : MonoBehaviourPunCallbacks
             
 
         }
-        else if (trap.gameObject.transform.parent.GetComponent<PhotonView>().Owner.ActorNumber!=PhotonNetwork.LocalPlayer.ActorNumber)
+        else if (trap.gameObject.transform.parent.GetComponent<PhotonView>().Owner.ActorNumber!=PhotonNetwork.LocalPlayer.ActorNumber&&iswritten)
         {
             Debug.Log("^is not mine");
             
@@ -444,15 +444,15 @@ public class PlayerBehaviour : MonoBehaviourPunCallbacks
                 // AddScore(trapType_score, trap.gameObject.GetComponentInParent<PhotonView>().Owner);
                 //  Debug.Log(" second set score success");
 
-                if(photonView.IsMine)
+                if(photonView.IsMine&&iswritten==false)
                 {
                     photonView.RPC("AddScore",
                               RpcTarget.AllBuffered,
                              trapType_score,
                              trap.gameObject.transform.parent.GetComponent<PhotonView>().Owner);
                 }
-               
-                
+
+              iswritten = true;
                 
 
                 Debug.Log(trap.gameObject.transform.parent.GetComponent<PhotonView>().Owner+"will be awarded score");
